ee5156c548b0b01fb712841d868229591bca7421
Mattia222 <127505321+Mattia222@users.noreply.github.com>
2023-04-23 10:35:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+   int n=0, primo=1, secondo=1, terzo=0, contatore=3;
+    cin >> n;
+
+    if(n>=2){
+        cout << 1 << endl;
+        cout << 1 << endl;
+        while(contatore<=n){
+            terzo=primo+secondo;
+            cout << terzo << endl;
+            secondo=primo;
+            primo=terzo;
+            contatore++;
+        }
+    }else{
+        cout << "errore" << endl;
+    }
    return 0;
 }
